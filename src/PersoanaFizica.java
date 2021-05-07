c61c2c37fd49688c4c4bd7f9c521edb6bd5a1111
Leonard Vlaicu <56519529+phayzee6002@users.noreply.github.com>
2021-05-07 15:43:30 +0300

    added source files

---
+++ src/PersoanaFizica.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class PersoanaFizica extends Client{
+	public PersoanaFizica() {
+	}
+	public PersoanaFizica(String nume, String prenume, String CNP, LocalDate dataNasterii, int ID) {
+		super(nume, prenume, CNP, dataNasterii, ID);
+	}
+}
